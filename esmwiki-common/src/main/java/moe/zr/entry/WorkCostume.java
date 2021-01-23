80227c66f3eee34a16137efd92285721914fdd41
ZhongRen <zhongren233@gmail.com>
2021-01-23 21:40:21 +0800

    [更新] 刷个版本号

---
+++ esmwiki-common/src/main/java/moe/zr/entry/WorkCostume.java
@@ -0,0 +1,36 @@
+package moe.zr.entry;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.var;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "workCostume")
+public class WorkCostume implements Serializable {
+    private static final long serialVersionUID = -3848147787378010808L;
+
+
+    @Id
+    String id;
+    String costumeName;
+    String image;
+    String character;
+    String series;
+    Integer active;
+    Integer passion;
+    Integer unique;
+    Integer smart;
+    Integer technique;
+    Integer charisma;
+    String appendDate;
+    String howToGet;
+
+}
