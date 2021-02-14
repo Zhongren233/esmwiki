dc08662e6c6fe7239159f5e86189e05d7eb8bdf5
ZhongRen <zhongren233@gmail.com>
2021-02-14 13:38:42 +0800

    [新增] 主动推送的测试类

---
+++ esmwiki-web/src/test/java/moe/zr/esmwiki/producer/service/impl/BotServiceImplTest.java
@@ -0,0 +1,19 @@
+package moe.zr.esmwiki.producer.service.impl;
+
+import moe.zr.qqbot.entry.SendMessage;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BotServiceImplTest {
+    @Autowired
+    BotServiceImpl botService;
+
+    @Test
+    void sendMessage() {
+        botService.sendMessage(new SendMessage().setMessage("测试发送信息").setGroupId(773891409));
+    }
+}
