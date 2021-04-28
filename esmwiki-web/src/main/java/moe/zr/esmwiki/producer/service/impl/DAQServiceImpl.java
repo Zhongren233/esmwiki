fc16367f5d5cb907496b0af861c6448a93881a1d
ZhongRen <zhongren233@gmail.com>
2021-04-28 10:23:42 +0800

    [优化] daq

--- esmwiki-web/src/main/java/moe/zr/esmwiki/producer/service/impl/DAQServiceImpl.java
+++ esmwiki-web/src/main/java/moe/zr/esmwiki/producer/service/impl/DAQServiceImpl.java
@@ -2,10 +2,14 @@ package moe.zr.esmwiki.producer.service.impl;
 
 import lombok.extern.slf4j.Slf4j;
 import moe.zr.esmwiki.producer.util.ReplyUtils;
+import moe.zr.pojo.PointRanking;
+import moe.zr.pojo.ScoreRanking;
 import moe.zr.qqbot.entry.IMessageQuickReply;
 import moe.zr.qqbot.entry.Message;
 import moe.zr.service.DAQService;
 import moe.zr.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.util.concurrent.ListenableFuture;
@@ -25,10 +29,13 @@ public class DAQServiceImpl implements DAQService, IMessageQuickReply {
     EventService eventService;
     final
     ReplyUtils replyUtils;
+    final
+    MongoTemplate mongoTemplate;
 
-    public DAQServiceImpl(EventService eventService, ReplyUtils replyUtils) {
+    public DAQServiceImpl(EventService eventService, ReplyUtils replyUtils, MongoTemplate mongoTemplate) {
         this.eventService = eventService;
         this.replyUtils = replyUtils;
+        this.mongoTemplate = mongoTemplate;
     }
 
     @Async
@@ -70,15 +77,29 @@ public class DAQServiceImpl implements DAQService, IMessageQuickReply {
         }
     }
 
+    public String dropCollection() {
+        mongoTemplate.dropCollection(PointRanking.class);
+        mongoTemplate.dropCollection(ScoreRanking.class);
+        return "删除集合成功";
+    }
+
     @Override
     public String onMessage(String[] str) {
+        switch (str[2]) {
+            case "get":
+                saveAllRanking();
+                return "手动进行爬榜";
+            case "drop":
+                return dropCollection();
+        }
+
         return null;
     }
 
     @Override
     public String onMessage(Message message) {
-        if (message.getGroupId()==773891409) {
-            saveAllRanking();
+        if (message.getGroupId() == 773891409) {
+            return onMessage(message.getRawMessage().split(" "));
         }
         return "喵呜";
     }
